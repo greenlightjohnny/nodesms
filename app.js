3767dab08bf16d06f259999657b6931648479a4e
fj <castelloiv@gmail.com>
2020-08-10 10:04:35 -0400

    Port change

--- app.js
+++ app.js
@@ -65,12 +65,12 @@ app.post("/", (req, res) => {
 });
 
 // Define port
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 //Start server
 
-const server = app.listen(port, () =>
-  console.log(`Server started on port ${port}`)
+const server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
 );
 
 /// Connect to socket.io
